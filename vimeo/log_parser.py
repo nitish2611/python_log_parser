eeb52412518de5f56c11b903c57488c864c50e20
NITISH VS <nitishvs@MacBook-Air.local>
2020-02-13 20:18:16 -0800

    feature to Handle few use cases

--- vimeo/log_parser.py
+++ vimeo/log_parser.py
@@ -12,7 +12,12 @@ from os import path
 
 domainErrors = {"total5xxCount":0}  #Initial counter for 5xx errors
 
-def main():
+def logParser():
+
+```
+Validates input parameters and calls calc5xxRespPercentage()
+input: <STARTTIME> <ENDTIME> </PATH/TO/LOGFILE/>
+```
 	if (len(sys.argv)-1 < 3):	#Check for required arguments
 		print ("Insufficient input parameters")
 		print("USAGE : python log_parser.py <STARTTIME> <ENDTIME> </PATH/TO/LOGFILE/>")
@@ -26,30 +31,35 @@ def main():
 				print("USAGE : python log_parser.py <STARTTIME> <ENDTIME> </PATH/TO/LOGFILE/>")
 			
 
-def calc5xxRespPercentage(startTime,EndTime,fileName):
-	print ("Between time "+str((sys.argv)[1])+" and time "+str((sys.argv)[2])+" :")
-	
+def calc5xxRespPercentage(startTime,EndTime,fileName):	
+
+```
+Calculates HTTP 5xx errors broken down by domain, for a given timeframe.
+ 
+```
 	with open(fileName, "r") as read_file:
+		print ("Between time "+str((sys.argv)[1])+" and time "+str((sys.argv)[2])+" in file "+fileName+":")
 		for line in read_file:
-			split_line_list = line.split('|')
-			logtime = float(split_line_list[0])
-			starttime = float(sys.argv[1])
-			endtime = float(sys.argv[2])
-			if (starttime <= logtime < endtime):
-				if (500 <= int(split_line_list[4].strip()) < 600):
-					domainErrors["total5xxCount"]+=1
-					if (split_line_list[2].strip() not in domainErrors.keys()):
-						domainErrors[split_line_list[2].strip()] = 1
-					else:
-						domainErrors[split_line_list[2].strip()]+=1
+			split_line_list = [line.strip() for line in line.split('|')]
+			if (len(split_line_list) != 1): 
+				logtime = float(split_line_list[0])
+				starttime = float(sys.argv[1])
+				endtime = float(sys.argv[2])
+				if (starttime <= logtime < endtime):				#Count number of 5xx
+					if (500 <= int(split_line_list[4].strip()) < 600):
+						domainErrors["total5xxCount"]+=1
+						if (split_line_list[2].strip() not in domainErrors.keys()): #Append to existing domain or create new domain key
+							domainErrors[split_line_list[2].strip()] = 1
+						else:
+							domainErrors[split_line_list[2].strip()]+=1
 	if (len(domainErrors)==1):
 		print("No 5xx errors were logged")
 	else:
 		for domain,count in domainErrors.items():
 			if domain != "total5xxCount":
-				print (str(domain)+" returned "+str((100*count)/domainErrors["total5xxCount"]) +"% 5xx errors")
+				print (str(domain)+" returned "+str((100*count)/domainErrors["total5xxCount"]) +"% 5xx errors") #Calculate percentage
 	
 					
 if __name__ == '__main__':
 	
-	main()
+	logParser()
--- vimeo/manifests/init.pp
+++ vimeo/manifests/init.pp
@@ -3,7 +3,7 @@ class vimeo (
 	file { 'log_parser_5xx':
 		ensure 	=> present,
 		path 	=> '/usr/local/bin/log_parser.py',
-		source 	=> 'python_log_parser:///vimeo/log_parser.py',
+		source 	=> '<PUPPETREPO>:///vimeo/log_parser.py',
 		owner 	=> 'root',
 		group 	=> 'root',
 		mode 	=> '0700',
